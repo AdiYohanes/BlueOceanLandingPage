054dcc0e3b2b5b78f3a38145af48def47a928d43
Adi Yohanes <adiyohanes19@gmail.com>
2024-03-21 17:05:18 +0700

    fix

--- components/Navbar/Logo.tsx
+++ components/Navbar/Logo.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Logo = () => {
   return <div>
     <Image 
-      src="/logo.png"
+      src="/images/logo.png"
       alt="logo"
       width={100}
       height={100}
